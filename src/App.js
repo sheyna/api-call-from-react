56290ee7eaee74bc59290217ff3bb7ffab622d2b
sheyna <sheynawatkins@gmail.com>
2022-03-20 23:52:06 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+
+    return (
+      <>
+        <h1>Data from an API</h1>
+        <form>
+          <button>Display Star Wars data</button>
+        </form>
+      </>
+    );
+  }
+}
+
+export default App;
